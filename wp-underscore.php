12099556f8e56699502b035dbedeb5801901c5d4
midwestE <midwestE@devbunker.com>
2021-11-11 13:52:44 -0600

    Remove old plugin code

--- wp-underscore.php
+++ wp-underscore.php
@@ -22,7 +22,7 @@ namespace _;
 define('WPUNDERSCORE', dirname(__FILE__));
 
 call_user_func(function () {
-    require_once WPUNDERSCORE . '/vendor/autoload.php';
+    //require_once WPUNDERSCORE . '/vendor/autoload.php';
     foreach (glob(WPUNDERSCORE . '/src/*.php') as $autoload) {
         require_once $autoload;
     }
