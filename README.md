6d5a3757750c46756f56981212a87d10784e6237
midwestE <midwestE@devbunker.com>
2021-11-11 13:25:27 -0600

    Add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# wp-underscore
+
+WP Underscore is a plugin for wordpress developers with helper libraries and additional functions.
--- wp-underscore.php
+++ wp-underscore.php
@@ -2,6 +2,23 @@
 
 namespace _;
 
+/*
+ *
+ * @link              https://github.com/midweste/wp-underscore
+ * @since             1.0.0
+ * @package           wp-underscore
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Underscore
+ * Plugin URI:        https://github.com/midweste/wp-underscore
+ * Description:       WP Underscore is a plugin for wordpress developers with helper libraries and additional functions.
+ * Version:           1.0.0
+ * Author:            midweste
+ * Author URI:        https://github.com/midweste/wp-underscore
+ * License:           GPL-2.0+
+ * Requires PHP:      7.2
+ */
+
 if (!defined('WPINC')) {
     die;
 }
