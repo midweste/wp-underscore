8093708ea3e566e43668ca85de4a52af4c739dfa
midwestE <midwestE@devbunker.com>
2022-02-26 07:34:25 -0600

    Add menu permission function return and doc

--- src/functions/menu.php
+++ src/functions/menu.php
@@ -2,7 +2,14 @@
 
 namespace _;
 
-function menu_set_submenu_permission( string $page, string $permission ) {
+/**
+ * Set permissions for a submenu item
+ *
+ * @param string $page Page slug of menu
+ * @param string $permission Capability permission to set
+ * @return bool True if successfully set, false if unable to set
+ */
+function menu_set_submenu_permission( string $page, string $permission ): bool {
 	global $submenu;
 	foreach ( $submenu as $m => &$s ) {
 		foreach ( $s as $index => &$mdef ) {
@@ -10,18 +17,27 @@ function menu_set_submenu_permission( string $page, string $permission ) {
 				continue;
 			}
 			$mdef[1] = $permission;
-			return;
+			return true;
 		}
 	}
+    return false;
 }
 
-function menu_set_permission( string $page, string $permission ) {
+/**
+ * Set permissions for a top level menu item
+ *
+ * @param string $page Page slug of menu
+ * @param string $permission Capability permission to set
+ * @return boolean True if successfully set, false if unable to set
+ */
+function menu_set_permission( string $page, string $permission ): bool {
 	global $menu;
 	foreach ( $menu as &$m ) {
 		if ( $page !== $m[2] ) {
 			continue;
 		}
 		$m[1] = $permission;
-		return;
+		return true;
 	}
+    return false;
 }
