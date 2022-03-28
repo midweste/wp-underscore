d4cc0da4ad60ef8441ff75f4d106a20f77ba2cfb
midwestE <midwestE@devbunker.com>
2022-03-28 15:38:01 -0500

    Add image functions and upload relative uri

--- src/functions/images.php
+++ src/functions/images.php
@@ -11,23 +11,36 @@ namespace _;
  *
  * @return array $image_sizes The image sizes
  */
-function image_styles_registered() {
-	global $_wp_additional_image_sizes;
+function image_styles_registered()
+{
+    global $_wp_additional_image_sizes;
 
-	$default_image_sizes = get_intermediate_image_sizes();
+    $default_image_sizes = get_intermediate_image_sizes();
+    $image_sizes = [];
+    foreach ($default_image_sizes as $size) {
+        $image_sizes[$size]['width']  = intval(get_option("{$size}_size_w"));
+        $image_sizes[$size]['height'] = intval(get_option("{$size}_size_h"));
+        $image_sizes[$size]['crop']   = get_option("{$size}_crop") ? get_option("{$size}_crop") : false;
+    }
 
-	$image_sizes = [];
-	foreach ( $default_image_sizes as $size ) {
-		$image_sizes[ $size ]['width']  = intval( get_option( "{$size}_size_w" ) );
-		$image_sizes[ $size ]['height'] = intval( get_option( "{$size}_size_h" ) );
-		$image_sizes[ $size ]['crop']   = get_option( "{$size}_crop" ) ? get_option( "{$size}_crop" ) : false;
-	}
+    if (isset($_wp_additional_image_sizes) && count($_wp_additional_image_sizes)) {
+        $image_sizes = array_merge($image_sizes, $_wp_additional_image_sizes);
+    }
+    // d($_wp_additional_image_sizes, $default_image_sizes, $image_sizes);
+    return $image_sizes;
+}
 
-	if ( isset( $_wp_additional_image_sizes ) && count( $_wp_additional_image_sizes ) ) {
-		$image_sizes = array_merge( $image_sizes, $_wp_additional_image_sizes );
-	}
-	// d($_wp_additional_image_sizes, $default_image_sizes, $image_sizes);
-	return $image_sizes;
+function image_styles_registered_common()
+{
+    $default_image_sizes = get_intermediate_image_sizes();
+
+    $image_sizes = [];
+    foreach ($default_image_sizes as $size) {
+        $image_sizes[$size]['width']  = intval(get_option("{$size}_size_w"));
+        $image_sizes[$size]['height'] = intval(get_option("{$size}_size_h"));
+        $image_sizes[$size]['crop']   = get_option("{$size}_crop") ? get_option("{$size}_crop") : false;
+    }
+    return $image_sizes;
 }
 
 /**
@@ -36,7 +49,73 @@ function image_styles_registered() {
  * @param string $slug
  * @return array
  */
-function image_style( string $slug ): array {
-	$styles = image_styles_registered();
-	return ( isset( $styles[ $slug ] ) ) ? $styles[ $slug ] : [];
+function image_style(string $slug): array
+{
+    $styles = image_styles_registered();
+    return (isset($styles[$slug])) ? $styles[$slug] : [];
+}
+
+/**
+ * Return the parent image of an image style without the -###x### dimension
+ *
+ * @param string $path_or_uri
+ * @return string
+ */
+function image_parent(string $path_or_uri): string
+{
+    $dimension_pattern = '/(.*?)\-\d*x\d*(\.\w*)$/';
+    $parent = preg_replace($dimension_pattern, '\1\2', $path_or_uri);
+    return $parent;
+}
+
+/**
+ * Return the post_id of the parent image linked to the path provided
+ *
+ * @param string $path_or_uri
+ * @return integer|null
+ */
+function image_parent_id(string $path_or_uri): ?int
+{
+    global $wpdb;
+    $parent = image_parent($path_or_uri);
+    $path_relative_uploads = ltrim(uploads_relative_uri($parent), '/');
+    $like = '%' . $wpdb->esc_like($path_relative_uploads) . '%';
+    $media_id = $wpdb->get_var($wpdb->prepare("SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key IN ('_wp_attached_file', '_wp_attachment_metadata') AND meta_value LIKE %s", $like));
+    return (is_numeric($media_id)) ? $media_id : null;
+}
+
+/**
+ * Return image style based on path or uri.  Will choose the first matching size with matching dimensions
+ *
+ * @param string $path_or_uri
+ * @return string
+ */
+function image_style_by_path(string $path_or_uri): string
+{
+    // check first if image is a generated type
+    $style = 'full';
+    $dimension_pattern = '/.*?\-(\d*)x(\d*)\.\w*$/';
+    preg_match($dimension_pattern, $path_or_uri, $matches);
+    if (empty($matches) || !is_numeric($matches[1]) || !is_numeric($matches[2])) {
+        return $style;
+    }
+    $width = (int) $matches[1];
+    $height = (int) $matches[2];
+
+    $parent_id = image_parent_id($path_or_uri);
+    if (!is_numeric($parent_id)) {
+        return $style;
+    }
+    $styles = image_styles_registered();
+    foreach ($styles as $name => $data) {
+        list($style_src, $style_width, $style_height) = image_downsize($parent_id, $name);
+        if ($path_or_uri === $style_src && $width === $style_width && $height === $style_height) {
+            return $name;
+        }
+        // $this_uri = wp_get_attachment_image_url($parent_id, $name);
+        // if ($path_or_uri === $this_uri) {
+        //     return $name;
+        // }
+    }
+    return $style;
 }
--- src/functions/uploads.php
+++ src/functions/uploads.php
@@ -2,10 +2,17 @@
 
 namespace _;
 
-function uploads_basedir(): string {
-	return \wp_get_upload_dir()['basedir'];
+function uploads_basedir(): string
+{
+    return \wp_get_upload_dir()['basedir'];
 }
 
-function uploads_relative_path( string $path ): string {
-	return str_replace( uploads_basedir(), '', $path );
+function uploads_relative_path(string $path): string
+{
+    return str_replace(uploads_basedir(), '', $path);
+}
+
+function uploads_relative_uri(string $uri): string
+{
+    return str_replace(path_relative(uploads_basedir()), '', uri_relative($uri));
 }
