47b07690d400444e9430ac877c9f5a09ded3de48
midwestE <midwestE@devbunker.com>
2022-04-11 09:07:48 -0500

    Add attachment change path

--- src/functions/attachment.php
+++ src/functions/attachment.php
@@ -9,20 +9,35 @@ function attachment_create(string $path): int
     }
 
     $filename    = basename($path);
-    $wp_filetype = wp_check_filetype($filename, null);
+    $wp_filetype = \wp_check_filetype($filename, null);
 
     $attachment = [
         'post_mime_type' => $wp_filetype['type'],
-        'post_title'     => sanitize_file_name($filename),
+        'post_title'     => \sanitize_file_name($filename),
         'post_content'   => '',
         'post_status'    => 'inherit',
     ];
 
-    $attach_id = wp_insert_attachment($attachment, $path);
+    $attach_id = \wp_insert_attachment($attachment, $path);
     if (!is_numeric($attach_id)) {
         throw new \Exception('Could not create attachment for %s', $path);
     }
     require_once ABSPATH . 'wp-admin/includes/image.php';
-    wp_generate_attachment_metadata($attach_id, $path);
+    \wp_generate_attachment_metadata($attach_id, $path);
     return $attach_id;
+}
+
+function attachment_change_path(int $attachment_id, string $filePath): bool
+{
+    $metadata = \wp_generate_attachment_metadata($attachment_id, $filePath);
+    //$id = \wp_update_attachment_metadata($this->getAttachmentId(), $metadata);
+    if (!is_array($metadata)) {
+        throw new \Exception(sprintf('Could not generate attachment metadata for %s', $filePath));
+    }
+    $result = \update_attached_file($attachment_id, $filePath);
+    if ($result === false) {
+        return false;
+    }
+    \clean_post_cache(\get_post($attachment_id));
+    return true;
 }
