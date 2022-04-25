720a10f613bb79cc8f88aa99e8802adcce14191a
midwestE <midwestE@devbunker.com>
2022-04-25 09:00:21 -0500

    Add strtotime in site timezone

--- src/functions/datetime.php
+++ src/functions/datetime.php
@@ -5,6 +5,15 @@ namespace _;
 /**
  * Return DateTimeZone in site timezone or America/Los_Angeles
  */
-function datetimezone( string $default = 'America/Los_Angeles' ): \DateTimeZone {
-	return new \DateTimeZone( get_option( 'timezone_string', $default ) );
+function datetimezone(string $default = 'America/Los_Angeles'): \DateTimeZone
+{
+    return new \DateTimeZone(get_option('timezone_string', $default));
+}
+
+function strtotime_timezoned(string $strtotime, string $format = 'U', string $timezone = '')
+{
+    $zone = ($timezone) ? $timezone : get_option('timezone_string', 'GMT');
+    $dt = new \DateTime('@' . strtotime($strtotime));
+    $dt->setTimeZone($zone);
+    return $dt->format($format);
 }
