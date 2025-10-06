64d3ca6a92a9c0a2cec45200b2b4c220a6271262
midwestE <midwestE@devbunker.com>
2025-10-06 08:31:34 -0500

    Add require for path to home

--- src/functions/path.php
+++ src/functions/path.php
@@ -2,15 +2,19 @@
 
 namespace _;
 
-function path_from_root( string $path ): string {
-	return str_replace( get_home_path(), '/', $path );
+function path_from_root(string $path): string
+{
+    require_once ABSPATH . 'wp-admin/includes/file.php';
+    return str_replace(\get_home_path(), '/', $path);
 }
 
-function path_to_uri( string $path ): string {
+function path_to_uri(string $path): string
+{
 	return WP_HOME . path_relative( $path );
 }
 
-function path_relative( string $path ): string {
+function path_relative(string $path): string
+{
 	$relative = '/' . str_replace( ABSPATH, '', $path );
 	return $relative;
 }
